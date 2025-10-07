98bfc909f20387692cad54a5e7800f7442980da8
Kirill Lapshakov <lapsakovkirill3@gmail.com>
2025-10-07 22:27:15 +0800

    add first lab

---
+++ app.js
@@ -0,0 +1,36 @@
+// Подключение модуля express
+var express = require("express");
+
+// Создание объекта  express
+var app = express();
+
+// Указание, что каталог public используется для хранения статических файлов
+app.use(express.static("public"));
+
+// Подключение шаблонизатора Pug.
+app.set("view engine", "pug");
+
+// Указание пути к каталогу, который хранит шаблоны в формате Pug.
+app.set("views", "./views");
+
+// Указание номера порта, через который будет запускаться приложение.
+app.listen(3000);
+
+// Определение обработчика для маршрута "/".
+// request — HTTP-запрос, свойствами которого являются строки запроса, параметры, тело запроса, заголовки HTTP.
+// response — HTTP-ответ, который приложение Express отправляет при получении HTTP-запроса.
+app.get("/", function(request, response)  {
+   // render() — функция, которая на основе шаблона (в данном случае шаблона index.pug) генерирует страницу html, которая отправляется пользователю.
+    response.render("index");
+});
+
+// Определение обработчикв для маршрута "/test"
+app.get("/test", function(request, response)  {
+   
+    response.render("test", {description: "Описание страницы"});
+});
+
+app.get("/information", function(request, response)  {
+ 
+  response.render("test", {description: "На этой странице будет описание проекта"});
+});
